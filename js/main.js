a92893c67da3ef8e2e6a451f00e55610933b8abf
Alex <64358227+ARCIKE@users.noreply.github.com>
2021-03-03 18:52:37 +0100

    Update

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 new fullpage('#fullpage', {
     navigation: true,
     responsiveWidth: 700,
-    anchors: ['home', 'about-me', 'contact'],
-    navigationTooltips: ['Home', 'About me', 'Contact'],
+    anchors: ['home', 'about-me', 'works'],
+    navigationTooltips: ['Home', 'About me', 'My Recent Works'],
     showActiveTooltip: true,
     parallax: true,
     scrollingSpeed: 3000,
     controlArrows: false,
     slidesNavigation: true,
     onLeave: function(origin, destination, direction){
-        console.log("Leaving section" + origin.index);
+        console.log("[ARCIKE] Leaving section" + origin.index);
     },
 });
